0f399f2174a450124f00d58ef179b5425b2c8fb0
ary <aryfasa@gmail.com>
2012-03-17 00:15:50 -0300

    terceiro commit

---
+++ ItemVenda.java
@@ -0,0 +1,52 @@
+package br.edu.fasa.vendas.domainModel;
+
+/**
+ * @author ary
+ */
+public class ItemVenda {
+    private int codigo;
+    private Venda venda;
+    private Produto produto;
+    private int quantidade;
+    private float valorVenda;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getValorVenda() {
+        return valorVenda;
+    }
+
+    public void setValorVenda(float valorVenda) {
+        this.valorVenda = valorVenda;
+    }
+
+    public Venda getVenda() {
+        return venda;
+    }
+
+    public void setVenda(Venda venda) {
+        this.venda = venda;
+    }
+}
