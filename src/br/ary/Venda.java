882c8c7c99d5b90cf2345e4a5635a0c6b0a92b51
santosary <aryfasa@gmail.com>
2012-04-21 13:01:48 -0300

    xx

--- src/br/ary/Venda.java
+++ src/br/ary/Venda.java
@@ -1,3 +1,4 @@
+
 package br.ary;
 
 import java.io.Serializable;
@@ -12,24 +13,24 @@ import javax.persistence.*;
 @Entity
 @Table (name = "Vendas")
 public class Venda implements Serializable {
-    
+
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column (name = "codigo")
     private int codigo;
-    
+
     @ManyToOne (cascade = CascadeType.PERSIST)
     @JoinColumn (name = "clientes")
     private Cliente cliente;
-    
+
     @OneToMany (cascade = CascadeType.ALL, mappedBy = "venda", targetEntity = ItemVenda.class)
     private List<ItemVenda> itens;
-            
-    @Temporal (TemporalType.TIMESTAMP)    
+
+    @Temporal (TemporalType.TIMESTAMP)
     private Date datas;
-                
+
     public Cliente getCliente() {
-        return cliente;        
+        return cliente;
     }
 
     public void setCliente(Cliente cliente) {
@@ -51,11 +52,11 @@ public class Venda implements Serializable {
     public void setData(Date data) {
         this.datas = data;
     }
-    
+
     public List<ItemVenda> getItens() {
         return itens;
     }
-    
+
     public void removeItens(ItemVenda iv) {
         itens.remove(iv);
     }
