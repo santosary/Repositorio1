91700b94c40f6cf45a65a38e84619810e5a3a252
santosary <aryfasa@gmail.com>
2012-04-21 12:58:18 -0300

    atualuzação

--- src/br/ary/Cliente.java
+++ src/br/ary/Cliente.java
@@ -6,20 +6,20 @@ import javax.persistence.*;
 
 /**
  * @author ary
- * @version 1.0 
+ * @version 1.0
  */
 @Entity
 @Table (name="Clientes")
 public class Cliente implements Serializable{
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "codigo")
     private int codigo;
-    
+
     @Column (name = "nome",length = 255)
     private String nome;
-    
+
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dtAninv;
 
@@ -38,11 +38,11 @@ public class Cliente implements Serializable{
     public void setNome(String nome) {
         this.nome = nome;
     }
-        
+
     public void setDtAninv(Date dataAninversario) {
         this.dtAninv = dataAninversario;
     }
-    
+
     public Date getDtAninv() {
         return dtAninv;
     }
